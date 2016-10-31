47c02ca21c8a45717d10cecb9d2cd1b9dedb9bb4
thibaut2g <thibs2g@gmail.com>
2016-10-31 17:29:30 +0100

    Mise en place des marqueurs sur la maps

--- js/map.js
+++ js/map.js
@@ -1,5 +1,4 @@
 function initialize() {
-
   var pyrmont = new google.maps.LatLng(50.631482, 3.059175);
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
@@ -9,8 +8,10 @@ function initialize() {
   });
 
   var myLatLng = new google.maps.LatLng(50.631482,3.059175);
+  var myLatLng1 = new google.maps.LatLng(50.63,3.07);
+  var myLatLng2 = new google.maps.LatLng(50.6,3.06);
 
-var contentString = '<div id="content">'+
+  var contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
       '<h3>Livraison</h3>'+
@@ -24,16 +25,91 @@ var contentString = '<div id="content">'+
     content: contentString
   });
 
-
   var marker = new google.maps.Marker({
           map: map,
           position: myLatLng,
-          title: 'Hello World!'
+          title: 'Hello World!',
+          icon : 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
         });
   marker.addListener('click', function() {
     infowindow.open(map, marker);
   });
+  marker.setMap(null);
+
+  var marker1 = new google.maps.Marker({
+          map: map,
+          position: myLatLng1,
+          title: 'Hello World!',
+          icon : 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+        });
+    marker1.setMap(null);
+
+  var marker2 = new google.maps.Marker({
+          map: map,
+          position: myLatLng2,
+          title: 'Hello World!',
+          icon : 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+        });
+  marker2.setMap(null);
+
+ document.getElementById('emplacement').onclick = function(){
+    marker.setMap(map);
+    marker1.setMap(null);
+    marker2.setMap(null);
+  };
+  document.getElementById('restaurant').onclick = function(){
+      marker.setMap(null);
+      marker1.setMap(map);
+      marker2.setMap(null);
+  };
+  document.getElementById('agriculteur').onclick = function(){
+      marker.setMap(null);
+      marker1.setMap(null);
+      marker2.setMap(map);
+  };
 
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+// function initCarte(){
+//   // init
+//   var i, posLat = 43.5, posLng = 0.6, nb = 600;
+//   var allMarkers = [], oMap, oMarker;
+//   // creation de la carte
+//   oMap = new google.maps.Map(document.getElementById('map-canvas'),{
+//       'backgroundColor': '#fff',
+//       'mapTypeControl':  false,
+//       'streetViewControl': false,
+//       'zoomControlOptions': {
+//         style: google.maps.ZoomControlStyle.SMALL
+//       },
+//       'zoom': 6,
+//       'center': new google.maps.LatLng( 46.80, 1.75),
+//       'mapTypeId': google.maps.MapTypeId.ROADMAP
+//     });
+//   // creation des marqueurs
+//   for( i=0; i <nb; i++){
+//     oMarker = new google.maps.Marker({
+//       'position' : new google.maps.LatLng( posLat, posLng),
+//       'map' : oMap
+//     });
+//     // décalage
+//     posLat += 0.01;
+//     posLng += 0.01;
+//     // stockage du marker
+//     allMarkers.push( oMarker);
+//   }
+//   // affectation des actions au boutons
+//   document.getElementById('btn_hide').onclick = function(){
+//     for( i=0; i< allMarkers.length; i++){
+//       allMarkers[i].setMap(null);
+//     }
+//   };
+//   document.getElementById('btn_show').onclick = function(){
+//     for( i=0; i< allMarkers.length; i++){
+//       allMarkers[i].setMap( oMap);
+//     }
+//   };
+// }
+// // init lorsque la page est chargee
+// google.maps.event.addDomListener(window, 'load', initCarte);
--- pages/accueil.php
+++ pages/accueil.php
@@ -4,7 +4,10 @@
 
 <div class="top"></div>
 <div class="container">
-	<h3>Trouver votre emplacement de livraison</h3>
+    <h5 style="display: inline-block"><b>Je cherche :</b></h5>
+   <button class="btn" id="emplacement">Un emplacement de livraison</button>
+   <button class="btn" id="restaurant">Un restaurant</button>
+   <button class="btn" id="agriculteur">Un agriculteur</button>
 
 	 
 </div>
